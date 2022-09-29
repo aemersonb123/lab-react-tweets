7af3c3591677af121dcf7793fea1bb0dc92974e5
Aiden Bostwick <aidenbostwick5@gmail.com>
2022-09-29 14:19:34 -0400

    done

--- src/App.js
+++ src/App.js
@@ -37,9 +37,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet) => 
+        <Tweet tweet={tweet} />
+        )}
     </div>
-  );
+    );
 }
+
 
 export default App;
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function ProfileImagee({imageUrl}) {
+    return <img src={imageUrl} alt='' className='profile'/>
+}
+export default ProfileImagee;
